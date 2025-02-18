657461a9a36196ece1fd502c50316efdbdfa441b
Tahira Ibrahim <160601974+Tahiraibrahim@users.noreply.github.com>
2025-02-18 18:31:35 +0500

    Add files via upload

---
+++ pythonclass-two.py
@@ -0,0 +1,16 @@
+# syntax
+
+print ("Hello World")
+print ("Class two contains syntex in python")
+
+# comments with example
+
+print("Adding two numbers") 
+x=10 #declared variable x
+y=20 #declared variable y
+print(x+y)
+
+# rapples
+
+# ctrl shift p ka zariye python start repl terminal ko open kare wo wihtin a secod answer daita hai
+
