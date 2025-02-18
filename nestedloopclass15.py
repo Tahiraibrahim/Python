e83548f788b4e96acb7bcc03ee244dcc46228429
Tahira Ibrahim <160601974+Tahiraibrahim@users.noreply.github.com>
2025-02-18 18:37:55 +0500

    Add files via upload

---
+++ nestedloopclass15.py
@@ -0,0 +1,9 @@
+# Nested loop in python 
+
+adj = ["red" , "big" , "tasty"]
+fruits = ["apple" , "banana" , "cheery"]
+for x in adj:
+    for y in fruits:
+        print(x,y)
+
+# aik hi loop ka andar do loop ko chalana
