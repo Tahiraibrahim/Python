4068f4186aa03c4a4997342b3bd9b4ade3560387
Tahira Ibrahim <160601974+Tahiraibrahim@users.noreply.github.com>
2025-02-18 18:32:40 +0500

    Add files via upload

---
+++ variablesclass3.py
@@ -0,0 +1,14 @@
+x,y,z ="orange" , "banana" , "kiwi"
+print(x)
+print(y)
+print(z)
+
+x,y,z ="2" , "5" , "6"
+print(x)
+print(y)
+print(z)
+
+first_number= 4
+second_number= 8
+print (first_number+second_number)
+
