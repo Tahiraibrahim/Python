38ba8790a5bc0cd77a50a1c41b738fdd26e1b35d
Tahira Ibrahim <160601974+Tahiraibrahim@users.noreply.github.com>
2025-02-18 18:34:55 +0500

    Add files via upload

---
+++ stringsclass8.py
@@ -0,0 +1,11 @@
+# strings
+
+# single line string "Python strings are immutable which means they cannot be changed after they are "created java strings also use this immutable style. Since strings cant be changed, we construct"
+# "new strings as we go to represent computed values"
+
+for x in "banana":
+    print(x)
+
+a = "Hello World!"
+print (len (a))
+
