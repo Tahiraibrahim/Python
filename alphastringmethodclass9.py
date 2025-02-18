8857597021fa39053b281a3098c96d8c06c8c28d
Tahira Ibrahim <160601974+Tahiraibrahim@users.noreply.github.com>
2025-02-18 18:35:28 +0500

    Add files via upload

---
+++ alphastringmethodclass9.py
@@ -0,0 +1,13 @@
+# python string is alpha () method.
+
+txt = "Company X"
+x = txt.isalpha()
+print(x)
+
+txt = "CompanyX"
+x = txt.isalpha()
+print(x)
+
+txt = "Company100"
+x = txt.isalpha()
+print(x)
